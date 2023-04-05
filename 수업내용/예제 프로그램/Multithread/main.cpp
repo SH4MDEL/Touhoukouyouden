3037b9d282ec11c96c23d1c117c7a8f1555aee2d
Seungjun Lee <beeutiful@naver.com>
2023-04-05 17:28:14 +0900

    23년 4월 4일 수업내용 추가
    . 멀티쓰레드 수업 내용, 실습 코드

---
+++ 수업내용/예제 프로그램/Multithread/main.cpp
@@ -0,0 +1,41 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+mutex m;
+volatile int sum = 0;
+
+void out_th(int threadid, int num_thread)
+{
+	for (int i = 0; i < 50000000 / num_thread; ++i) {
+		sum += 2;
+	}
+}
+
+int main()
+{
+	//register int sum = 0;
+	for (int num_thread = 1; num_thread <= 16; num_thread *= 2) {
+		sum = 0;
+		vector<thread> workers;
+
+		auto start_t = high_resolution_clock::now();
+		for (int i = 0; i < num_thread; ++i)
+			workers.emplace_back(out_th, i, num_thread);
+		for (auto& t : workers) t.join();
+		auto end_t = high_resolution_clock::now();
+
+		auto exec_t = end_t - start_t;
+		auto exec_ms = duration_cast<milliseconds>(exec_t).count();
+
+		cout << sum << ", " << exec_ms << endl;
+	}
+	//int num_core = thread::hardware_concurrency();
+	//// 논리 프로세서의 개수를 받아온다.
+
+
+}
